87f668ae945c29dd55599c9addbf4fb44244f01e
rohit kumar <rohit1071998@gmail.com>
2020-06-19 10:16:29 +0530

    Add Cart

---
+++ src/Cart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CartItems from './CartItem';
+
+
+class Cart extends React.Component {
+    render() {
+        return (
+            <div className="cart">
+            <CartItems />
+            <CartItems />
+            <CartItems />
+            </div> 
+        )
+    }
+}
+
+export default Cart;
